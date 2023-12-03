d19a283ffb40263cc825bbdaf554e0cd0e7e42d2
BawarHamid <bawar@live.dk>
2023-12-03 01:15:23 +0100

    ui changes.
    border

--- client/src/components/cards/OptionCard.tsx
+++ client/src/components/cards/OptionCard.tsx
@@ -5,7 +5,8 @@ type OptionCardProps = {
 
 const OptionCard: React.FC<OptionCardProps> = ({ cardTitle, cardParagraf }) => {
   return (
-    <button className="py-5 px-8 my-2 hover:bg-slate-50 focus:border-[2px] focus:border-blue-600 focus:bg-slate-50 rounded-lg shadow-lg shadow-[#E4E3E9] max-w-sm transform motion-safe:hover:scale-105">
+    <button className="outline-none py-5 px-8 my-2 focus:border focus:border-blue-600 focus:bg-slate-50 rounded-lg shadow-lg shadow-[#E4E3E9] max-w-sm transform motion-safe:focus:scale-105">
+      {/* <button className="outline-none py-5 px-8 my-2 hover:bg-slate-50 focus:border-[2px] focus:border-blue-600 focus:bg-slate-50 rounded-lg shadow-lg shadow-[#E4E3E9] max-w-sm transform motion-safe:hover:scale-105"> */}
       <h1 className="text-xl font-bold text-black text-start">{cardTitle}</h1>
       <p className="text-xs text-gray-600 text-start font-bold italic">
         {cardParagraf}
